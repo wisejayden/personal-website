b822ddb90f6320962afbdbf9c115e63215aa5c9a
Jayden Wise <jayden.wise@hotmail.com>
2018-04-20 16:51:54 +0200

    Implemented glitch effect

--- js/script.js
+++ js/script.js
@@ -3,34 +3,45 @@
         trigger = container.querySelector( 'button.trigger' );
         var storyTrigger = document.getElementById( 'my-story');
         var contactTrigger = document.getElementById( 'contact-trigger');
+        var glitch = document.getElementsByClassName( 'demo-2');
+        var imgLoaded = document.getElementsByClassName( 'imgloaded ');
+
+        var glitchStyle = window.getComputedStyle(glitch[0]);
+        var glitchAnimation = glitchStyle.getPropertyValue('--time-anim');
+        console.log("GLITCH ANIMATION", glitchAnimation);
 
 
+        // function changeAnimation() {
+        //     glitch[0].style.animationDuration= "0s";
+        //     console.log("CHanged Duration!", glitch[0].style);
+        //     imgLoaded[0].style.animationDuration = "0s";
+        //
+        // }
 
     function toggleContent() {
         if( classie.has( container, 'container--open' ) ) {
             classie.remove( container, 'container--open' );
             classie.remove( trigger, 'trigger--active' );
             window.addEventListener( 'scroll', noscroll );
+            glitch[0].style.setProperty('--time-anim', '4s');
+
+
         }
         else {
             classie.add( container, 'container--open' );
             classie.add( trigger, 'trigger--active' );
+            classie.remove( container, 'glitch');
             window.removeEventListener( 'scroll', noscroll );
+            // changeAnimation();
+            glitch[0].style.setProperty('--time-anim', '0s');
+
         }
     }
 
-    function toggleContact() {
-        if( classie.has( container, 'contac--open' ) ) {
-            classie.remove( container, 'contact--open' );
-            classie.remove( contactTrigger, 'contact-trigger--active' );
-            window.addEventListener( 'scroll', noscroll );
-        }
-        else {
-            classie.add( container, 'contact--open' );
-            classie.add( contactTrigger, 'contact-trigger--active' );
-            window.removeEventListener( 'scroll', noscroll );
-        }
-    }
+
+
+    // document.getElementById("p2").style.color = "blue";
+
 
     function noscroll() {
         window.scrollTo( 0, 0 );
@@ -43,7 +54,11 @@
     window.addEventListener( 'scroll', noscroll );
 
     trigger.addEventListener( 'click', toggleContent );
-    contactTrigger.addEventListener( 'click', toggleContact );
+    // contactTrigger.addEventListener( 'click', toggleContact );
+    // glitch[0].addEventListener( 'click', function() {
+    //     console.log("HELLO!");
+    // } );
+
 
 
     // For Demo purposes only (prevent jump on click)
