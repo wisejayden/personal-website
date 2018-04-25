7e879c89f410d666853703e0533bc37bddef2658
Jayden Wise <jayden.wise@hotmail.com>
2018-04-25 20:26:12 +0200

    Made the about page responsive

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,8 @@
         var glitch = document.getElementsByClassName( 'demo-2');
         var imgLoaded = document.getElementsByClassName( 'imgloaded ');
         var body = document.getElementsByTagName("BODY")[0];
+        var portfolio = document.getElementById('portfolio');
+
 
 
         var glitchStyle = window.getComputedStyle(glitch[0]);
@@ -19,12 +21,17 @@
         //     imgLoaded[0].style.animationDuration = "0s";
         //
         // }
+        portfolio.addEventListener( 'click', toggleContent );
+
 
     function toggleContent() {
+        //Close portfolio
         if( classie.has( container, 'container--open' ) ) {
             classie.remove( container, 'container--open' );
             classie.remove( trigger, 'trigger--active' );
             window.addEventListener( 'scroll', noscroll );
+            portfolio.innerHTML = "Portfolio";
+
             glitch[0].style.setProperty('--time-anim', '4s');
             body.classList.add("stop-scrolling");
             //For Moble devices
@@ -37,12 +44,15 @@
 
         }
         else {
-
+            //Open Portfolio
             classie.add( container, 'container--open' );
             classie.add( trigger, 'trigger--active' );
             classie.remove( container, 'glitch');
+            portfolio.innerHTML = "Close";
+
             window.removeEventListener( 'scroll', noscroll );
             // changeAnimation();
+
             glitch[0].style.setProperty('--time-anim', '0s');
             body.classList.remove("stop-scrolling");
             //for mobile devices
@@ -68,7 +78,7 @@
     // disable scrolling
     window.addEventListener( 'scroll', noscroll );
 
-    trigger.addEventListener( 'click', toggleContent );
+    portfolio.addEventListener( 'click', toggleContent );
     // contactTrigger.addEventListener( 'click', toggleContact );
     // glitch[0].addEventListener( 'click', function() {
     //     console.log("HELLO!");
---
+++ public/js/script2.js
@@ -0,0 +1,6 @@
+var email = document.getElementsByClassName( 'email ');
+
+email[0].addEventListener( 'click', function() {
+    var copyText = "jayden.wise@hotmail.com";
+
+} );
